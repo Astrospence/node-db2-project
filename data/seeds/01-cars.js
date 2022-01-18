1c814d0c86bf9305785a3e62e4f86e3443b45781
Astrospence <illustrator.wood@gmail.com>
2022-01-18 14:01:29 -0700

    fleshed out seed file

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,32 @@
 // STRETCH
+const cars = [
+    {
+        vin: '7777777777777',
+        make: 'Ford',
+        mode: 'Bronco',
+        mileage: 20,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '7777777777777',
+        make: 'Toyota',
+        mode: 'Camry',
+        mileage: 40,
+        title: 'salvage',
+        transmission: 'automatic'
+    },
+    {
+        vin: '7777777777777',
+        make: 'Dodge',
+        mode: 'Dart',
+        mileage: 30,
+        title: 'clean',
+        transmission: 'manual'
+    }
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
