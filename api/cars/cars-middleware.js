ef7195a8d9fa5dab02a153201e449f17193cff0d
Astrospence <illustrator.wood@gmail.com>
2022-01-18 20:24:49 -0700

    getById endpoint working

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,16 @@
+const Cars = require('./cars-model')
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+  Cars.getById(req.params.id)
+    .then(car => {
+      if (!car) {
+        next({ status: 404, message: `car with id ${req.params.id} is not found` })
+      } else {
+        req.car = car
+        next()
+      }
+    })
+    .catch(next)
 }
 
 const checkCarPayload = (req, res, next) => {
@@ -12,4 +23,11 @@ const checkVinNumberValid = (req, res, next) => {
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,10 +1,19 @@
 const router = require('express').Router()
 const Cars = require('./cars-model')
+const { checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique } = require('./cars-middleware')
 
 router.get('/', (req, res, next) => {
     Cars.getAll()
         .then(cars => {
             res.status(200).json(cars)
+        })
+        .catch(next)
+})
+
+router.get('/:id', checkCarId, (req, res, next) => {
+    Cars.getById(req.params.id)
+        .then(car => {
+            res.status(200).json(car)
         })
         .catch(next)
 })
