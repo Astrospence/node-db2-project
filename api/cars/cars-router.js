fbba55dd656a1c486067240371e8d5535ecf23a9
Astrospence <illustrator.wood@gmail.com>
2022-01-18 19:59:10 -0700

    getAll endpoint working

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,12 @@
-// DO YOUR MAGIC
+const router = require('express').Router()
+const Cars = require('./cars-model')
+
+router.get('/', (req, res, next) => {
+    Cars.getAll()
+        .then(cars => {
+            res.status(200).json(cars)
+        })
+        .catch(next)
+})
+
+module.exports = router
